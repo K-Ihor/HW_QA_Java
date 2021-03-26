b0b777020c6b086eae8872a2a33e1ea27bf8103c
K-Ihor <i.kucherov.m@gmail.com>
2021-03-26 12:03:50 +0200

    Test_2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -9,4 +9,17 @@ public class MainClassTest extends MainClass
         Assert.assertTrue("getLocalNumber not return 14", this.getLocalNumber() == 14);
     }
 
+    @Test
+    public void  testGetClassNumber()
+    {
+        if (this.getClassNumber() > 45){
+            System.out.println("ok");
+        }else if(this.getClassNumber() < 45) {
+            System.out.println("getClassNumber < 45");
+        }else{
+            System.out.println("getClassNumber == 45");
+        }
+
+        Assert.assertTrue("getClassNumber !> 45",this.getClassNumber() > 45);
+    }
 }
