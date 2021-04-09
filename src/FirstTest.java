1097e0c599422b682f8f449b38aa2e9e061c02b4
K-Ihor <i.kucherov.m@gmail.com>
2021-04-09 13:35:52 +0200

    assertElementHesText

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -38,6 +43,55 @@ public class FirstTest
     public void firstTest()
     {
         System.out.println("First test run");
+    }
+
+    @Test
+    public void check_text()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Can`t find search Wikipedia input",
+                5
+        );
+
+        assertElementHesText(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Search…",
+                "We see unexpected 'text'",
+                15
+        );
+
+    }
+
+    private void assertElementHesText(By by, String text, String error_message, long timeoutInSeconds)
+    {
+        WebElement waitSearchLine = waitForElementPresent(by, error_message, timeoutInSeconds);
+
+        String article_title = waitSearchLine.getAttribute("text");
+        Assert.assertEquals(
+                error_message,
+                text,
+                article_title
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message)  // перегрузка метода с выносом таймаута
+    {
+        return waitForElementPresent(by, error_message, 5);
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, 5);
+        element.click();
+        return element;
     }
 }
 
